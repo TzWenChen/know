6c477c077d14e6c7b5fa40837c2de2b643d7d99f
WeiJane <rabbitjaneson@gmail.com>
2016-12-27 10:31:14 +0800

    Edit SingleTestActivity.java, a can-played version

--- app/src/main/java/com/knowmemo/usermanagement/SingleTestActivity.java
+++ app/src/main/java/com/knowmemo/usermanagement/SingleTestActivity.java
@@ -10,14 +10,20 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.os.CountDownTimer;
 
-import sqlite.SqlHelper;
 import sqlite.Words;
 import sqlite.tableDao;
 import sqlite.GameQuestion;
 
 import org.w3c.dom.Text;
 
+import java.lang.reflect.Array;
+import java.util.HashSet;
+import java.util.Random;
+
 public class SingleTestActivity extends AppCompatActivity{
+    tableDao tabledao;
+    GameQuestion currentQ = null;
+    int correct = 0;
     TextView question;
     TextView timer;
     TextView record;
@@ -25,6 +31,7 @@ public class SingleTestActivity extends AppCompatActivity{
     Button btn_ans2;
     Button btn_ans3;
     Button btn_ans4;
+    Button btn_skip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +39,11 @@ public class SingleTestActivity extends AppCompatActivity{
         setContentView(R.layout.single_test);
 
         this.layoutInit();
-        timeCountDown();
+        this.databaseInit();
+        this.timeCountDown();
+        this.listenerInit();
+        this.insertQuestion();
+        this.insertAnswer();
     }
 
     @Override
@@ -55,10 +66,59 @@ public class SingleTestActivity extends AppCompatActivity{
         btn_ans2 = (Button) findViewById(R.id.btn_ans2);
         btn_ans3 = (Button) findViewById(R.id.btn_ans3);
         btn_ans4 = (Button) findViewById(R.id.btn_ans4);
+        btn_skip = (Button) findViewById(R.id.btn_single_skip);
     }
 
     private void databaseInit() {
+        tabledao = new tableDao(getApplicationContext());
+        if (tabledao.getWordsCount() == 0) {
+            tabledao.sampleWord();
+        }
+        if (tabledao.getGameQuestionCount() == 0) {
+            tabledao.sampleQuestion();
+        }
+    }
 
+    private void listenerInit() {
+        btn_skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //初始化
+                currentQ = null;
+                //置入下一題
+                insertQuestion();
+                //置入答案
+                insertAnswer();
+            }
+        });
+
+        btn_ans1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnClick(btn_ans1.getText().toString());
+            }
+        });
+
+        btn_ans2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnClick(btn_ans2.getText().toString());
+            }
+        });
+
+        btn_ans3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnClick(btn_ans3.getText().toString());
+            }
+        });
+
+        btn_ans4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnClick(btn_ans4.getText().toString());
+            }
+        });
     }
 
     private void timeCountDown() {
@@ -74,5 +134,81 @@ public class SingleTestActivity extends AppCompatActivity{
                 timer.setText("" + millisUntilFinished/1000);
             }
         }.start();
+    }
+
+    public void insertQuestion() {
+        int questionCnt = tabledao.getGameQuestionCount();
+        long seed = System.nanoTime();
+        Random random = new Random(seed);
+        int q_id= random.nextInt(questionCnt) + 1;
+        currentQ = tabledao.getQuestionById(q_id);
+        question.setText(currentQ.getQuestion());
+    }
+
+    public void insertAnswer() {
+        String[] answerList = new String[4];
+        //初始化答案陣列
+        for (int i = 0; i < answerList.length; i++) {
+            answerList[i] = null;
+        }
+        int right_ans_id = currentQ.getAnswer_id();
+        String right_answer = tabledao.getWordById(right_ans_id).getWord();
+        long seed = System.nanoTime();
+        Random random = new Random(seed);
+        int right_ans_index = random.nextInt(4);
+        //先填入正確答案id的按鈕位置
+        answerList[right_ans_index] = right_answer;
+        //產生其他答案
+        int[] otherAnswers = pickOtherAnswers(right_ans_id);
+        for (int i = 0, j = 0; i < 4 && j < otherAnswers.length; i++) {
+            if (answerList[i] == null && otherAnswers[j] != right_ans_id) {
+                answerList[i] = tabledao.getWordById(otherAnswers[j]).getWord().toString();
+                j++;
+            }
+        }
+
+        btn_ans1.setText(answerList[0]);
+        btn_ans2.setText(answerList[1]);
+        btn_ans3.setText(answerList[2]);
+        btn_ans4.setText(answerList[3]);
+    }
+
+    private int[] pickOtherAnswers(int right_ans_id) {
+        int[] otherAnswerList = new int[3];
+        long seed = System.nanoTime();
+        HashSet set = new HashSet<Integer>(3);
+        set.add(right_ans_id);
+        for (int i = 0; i < otherAnswerList.length; i++) {
+            Random random = new Random(seed);
+            int tmp = random.nextInt(tabledao.getWordsCount()) + 1;
+            while (!set.add(tmp)) {
+                tmp = random.nextInt(tabledao.getWordsCount()) + 1;
+            }
+            otherAnswerList[i] = tmp;
+        }
+        return otherAnswerList;
+    }
+
+    private boolean checkAnswer(String chosenAnswer) {
+        boolean isCorrect = false;
+        String rightAnswer = tabledao.getWordById(currentQ.getAnswer_id()).getWord().toString();
+        if (chosenAnswer != null && chosenAnswer.toLowerCase().equals(rightAnswer)) {
+            isCorrect = true;
+        } else {
+            isCorrect = false;
+        }
+        return isCorrect;
+    }
+
+    private void btnClick(String btnText) {
+        if (checkAnswer(btnText.toLowerCase())) {
+            correct++;
+            record.setText(correct + "");
+            insertQuestion();
+            insertAnswer();
+        } else {
+            insertQuestion();
+            insertAnswer();
+        }
     }
 }
