3928fd3677e114bad182192b946799262e116484
WeiJane <rabbitjaneson@gmail.com>
2016-12-29 02:28:00 +0800

    Edit SingleRankActivity.java

--- app/src/main/java/com/knowmemo/usermanagement/SingleRankActivity.java
+++ app/src/main/java/com/knowmemo/usermanagement/SingleRankActivity.java
@@ -4,12 +4,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import sqlite.tableDao;
 
 public class SingleRankActivity extends AppCompatActivity {
+    ListView game_record_listView;
+    tableDao tabledao;
+    ArrayList<HashMap<String, Object>> top10Record;
+    SimpleAdapter simpleAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.single_rank);
+
+        tabledao = new tableDao(getApplicationContext());
+        game_record_listView = (ListView) findViewById(R.id.game_record_list);
+        top10Record = tabledao.getTop10GameRecord();
+
+        this.insertRecord();
     }
 
     @Override
@@ -21,5 +40,12 @@ public class SingleRankActivity extends AppCompatActivity {
             this.finish();
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    private void insertRecord() {
+        simpleAdapter = new SimpleAdapter(this,
+                top10Record, R.layout.game_record_item_layout, new String[]{"排名", "答題數", "日期"},
+                new int[]{R.id.col_num, R.id.col_grade, R.id.col_date});
+        game_record_listView.setAdapter(simpleAdapter);
     }
 }
